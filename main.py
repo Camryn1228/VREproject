2d24bdd667f79ce46d5e418e7b7e8ad85cc2f58e
aariii <85893087+aariii@users.noreply.github.com>
2021-07-19 08:15:30 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+"""User input"""
+print("Open the terminal at the bottom of the screen.") #User opens terminal
+print("Print the follwoing command: pip install pyinstaller") # User follows commmand
+print("Print pyinstaller main.py --onefile") #User follows command
+
+
+
