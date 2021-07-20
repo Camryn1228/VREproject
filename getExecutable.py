4b5100b3915fd025335d01d3d9e4c1d59d8b68e3
aariii <85893087+aariii@users.noreply.github.com>
2021-07-20 11:08:58 -0500

    Delete getExecutable.py

--- getExecutable.py
+++
@@ -1,5 +0,0 @@
-import os #code that imports that os class
-files_and_directories = os.listdir("C:/Users/noeli/PycharmProjects/Executables folder") #Folder I created to test the code
-print(f"List of executables in the folder: {files_and_directories}")
-print("File folder has been opened.")
-os.startfile("C:/Users/noeli/PycharmProjects/Executables folder") #Opens up the file folder
