d609345b0fcc431e182c71af11e088d27c208c6f
aariii <85893087+aariii@users.noreply.github.com>
2021-07-22 16:21:25 -0500

    Add files via upload

--- getExecutables.py
+++ getExecutables.py
@@ -1,13 +1,19 @@
 """Imported the os class
-'path_main variable stores the file folder I created
-'files_and_directories is the list
- For loop iterates through the variables
- For loop also opens the file folder on
-The 'Executables' folder is then extracted and read using the variables that are stored in the main file"""
+'exe_path' variable stores user input
+'files_and_directories' lists 'exe_path'
+ For loop iterates through the variables"""
 import os
-path_main =("C:/Users/noeli/PycharmProjects/Executables folder")#File folder I created to test code.
-files_and_directories = os.listdir(path_main)
 
-for file in files_and_directories:
-    filepath = path_main+ "/" + file
-os.startfile("C:/Users/noeli/PycharmProjects/Executables folder")
+
+def get_executables():
+    exe_path = input("Enter the path of your executable: ")  # path to executable ++.
+    files_and_directories = os.listdir(exe_path)
+
+    for file in files_and_directories:
+        filepath = exe_path+ "/" + file
+        ExtactingData.extract(exe_path)
+        ImportsandExports.run(exe_path)
+
+
+
+get_executables()
