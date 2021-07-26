6eb3271d868b7f82b633be753927c42454ad2f1f
cherithkay <85884544+cherithkay@users.noreply.github.com>
2021-07-26 15:34:23 -0400

    Delete main1.py

--- main1.py
+++
@@ -1,9 +0,0 @@
-import ExtactingData
-import ImportsandExports
-
-if __name__=='__main__':
-
-    exe_path = input("Enter the path of your executable: ")  # path to executable ++.
-
-    ExtactingData.extract(exe_path)
-    ImportsandExports.run(exe_path)
